dbfdb67149810ec8dc55c1c878d1bc9b019a83b4
huy165273 <21020765@vnu.edu.vn>
2022-03-30 19:07:01 +0700

    Bài tập lớn

--- Game2048/2048.cpp
+++ Game2048/2048.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
 #include <fstream>
 #include <ctime>
 #include <cstdlib>
 #include <vector>
+#include "array_manipulation.h"
 using namespace std;
+
 const int SCREEN_WIDTH = 450;
 const int SCREEN_HEIGHT = 600;
 const int FIRST_NUMBER_COORDINATE_X = 25;
@@ -14,24 +15,14 @@ const int SIZE_NUMBER = 100;
 const int amount_image = 13;
 
 const int size_max = 10;
+int **array= new int *[size_max];
+int **arrayBefore= new int *[size_max];
 int gameSize;
-int array[size_max][size_max];
-int arrayBefore[size_max][size_max];
-void khoiTaoMang();
-int enterGameSize();
 void randomNumber();
-char enterKey();
 bool gameOver();
 bool win();
-void updateArray(const char &key);
-bool compareArray(int (*arrayBefore)[size_max]); // so sánh 2 mảng có giống nhau k
-void updateArrayUp();
-void updateArrayDown();
-void updateArrayRight();
-void updateArrayLeft();
-void printArray();
-void printResult(const char &checkGameOver, const char &checkWin);
-void updateArrayBefore(int (*arrayBerfore)[size_max]);
+void khoiTaoMang();
+int enterGameSize();
 void playGame();
 
 bool init();
@@ -71,44 +62,33 @@ int main(int arc, char *argv[])
                         }
                         else if (e.type == SDL_KEYDOWN)
                         {
-                            updateArrayBefore(arrayBefore);
+                            updateArrayBefore(arrayBefore, array, gameSize);
                             switch (e.key.keysym.sym)
                             {
                             case SDLK_UP: // xử lý hàm khi di chuyển lên
-                                updateArrayUp();
+                                updateArrayUp(array, gameSize);
                                 break;
                             case SDLK_DOWN: // xử lý hoàm khi di chuyển xuống
-                                updateArrayDown();
+                                updateArrayDown(array, gameSize);
                                 break;
                             case SDLK_LEFT: // xử lý hàm khi di chuyển sang trái
-                                updateArrayLeft();
+                                updateArrayLeft(array, gameSize);
                                 break;
                             case SDLK_RIGHT: // xử lý hàm khi di chuyển sang phải
-                                updateArrayRight();
+                                updateArrayRight(array, gameSize);
                                 break;
                             }
-                            if (!compareArray(arrayBefore))
+                            if (!compareArray(arrayBefore, array, gameSize))
                                 randomNumber();
                         }
                     }
 
-                    SDL_Rect stretch[0]; // kéo căng ra
-                    stretch[0].x = 0;
-                    stretch[0].y = 0;
-                    stretch[0].h = SCREEN_HEIGHT;
-                    stretch[0].w = SCREEN_WIDTH;
-                    SDL_BlitScaled(gDisplaySurface[0], NULL, gScreenSurface, &stretch[0]);
-                    /*for (int i = 1; i <= 3; i++)
-                    {
-                        for (int j = 1; j <= 4; j++)
-                        {
-                            stretch[i].x = FIRST_NUMBER_COORDINATE_X + SIZE_NUMBER * (j - 1);
-                            stretch[i].y = FIRST_NUMBER_COORDINATE_Y + SIZE_NUMBER * (i - 1);
-                            stretch[i].h = SIZE_NUMBER;
-                            stretch[i].w = SIZE_NUMBER;
-                            SDL_BlitScaled(gDisplaySurface[i], NULL, gScreenSurface, &stretch[i]);
-                        }
-                    }*/
+                    SDL_Rect stretch; // kéo căng ra
+                    stretch.x = 0;
+                    stretch.y = 0;
+                    stretch.h = SCREEN_HEIGHT;
+                    stretch.w = SCREEN_WIDTH;
+                    SDL_BlitScaled(gDisplaySurface[0], NULL, gScreenSurface, &stretch);
                     screenSurface();
                     SDL_UpdateWindowSurface(gWindow);
                 }
@@ -250,23 +230,11 @@ void screenSurface(){
 //
 
 void playGame()
-{
-    //bool checkGameOver;
-    //bool checkWin;
+{   
     gameSize = enterGameSize();
     khoiTaoMang();
     randomNumber();
     randomNumber();
-    /*do
-    {
-        key = enterKey();
-        updateArrayBefore(arrayBefore);
-        updateArray(key);
-        if (!compareArray(arrayBefore))
-            randomNumber();
-        printArray();
-    } while (!gameOver() || win());
-    printResult(checkGameOver, checkWin);*/
 }
 int enterGameSize()
 {
@@ -274,43 +242,35 @@ int enterGameSize()
 }
 void randomNumber()
 {
-    bool check = true;
-    int x, y;
-    while (check)
-    {
-        srand(time(nullptr));
-        x = rand() % gameSize;
-        y = rand() % gameSize;
-        if (array[x][y] == 0)
-        {
-            // chọn số cho ô 2 hoặc 4
-            // tạm thời chọn tỉ lệ ra số 4 là 10%
-            int randomNumber;
-            randomNumber = (rand() % 10);
-            if (randomNumber == 0)
-                array[x][y] = 4;
-            else
-                array[x][y] = 2;
-            check = false;
-        }
-    }
-}
-void khoiTaoMang()
-{
+    int count1 = 0, count2 = 0;
     for (int i = 0; i < gameSize; i++)
     {
         for (int j = 0; j < gameSize; j++)
-        {
-            array[i][j] = 0;
-        }
+            if (array[i][j] == 0)
+                count1++;
+    }
+    srand(time(nullptr));
+    int random = rand() % count1 + 1;
+    for (int i = 0; i < gameSize; i++)
+    {
+        for (int j = 0; j < gameSize; j++)
+            if (array[i][j] == 0)
+            {
+                count2++;
+                if (count2 == random)
+                {
+                    // chọn số cho ô 2 hoặc 4
+                    // tạm thời chọn tỉ lệ ra số 4 là 10%
+                    int randomNumber = (rand() % 10);
+                    if (randomNumber == 0)
+                        array[i][j] = 4;
+                    else
+                        array[i][j] = 2;
+                }
+            }
     }
 }
-char enterKey()
-{
-    char key;
-    cin >> key;
-    return key;
-}
+
 bool gameOver()
 {
     for (int i = 0; i < gameSize; i++)
@@ -335,207 +295,11 @@ bool win()
     }
     return false;
 }
-void printResult(const char &checkGameOver, const char &checkWin)
-{
-    if (checkGameOver)
-    {
-        cout << "You lost!" << endl;
-    }
-    if (checkWin)
-    {
-        cout << "Congratulation! You win!" << endl;
-    }
-}
-void printArray()
-{
-    for (int i = 1; i < 50; i++)
-        cout << endl;
-    for (int i = 0; i < gameSize; i++)
-    {
-        for (int j = 0; j < gameSize; j++)
-        {
-            cout << array[i][j] << "  ";
-        }
-        cout << endl;
-    }
-}
-void updateArray(const char &key)
-{
-    switch (key)
-    {
-    case 'w':
-        updateArrayUp();
-        break;
-    case 's':
-        updateArrayDown();
-        break;
-    case 'd':
-        updateArrayRight();
-        break;
-    case 'a':
-        updateArrayLeft();
-        break;
-    }
-}
-void updateArrayUp()
-{
-    // cout<<"up"<<endl;
-    for (int j = 0; j < gameSize; j++)
-    {
-        vector<bool> arrayCheck(size_max, true); // xác định xem vị trí đấy có đc cộng k
-        bool check = false;                      // check cột hợp lệ chưa
-        while (!check)
-        {
-            for (int i = 0; i <= gameSize - 2; i++)
-            {
-                if (array[i][j] == 0 && array[i + 1][j] != 0)
-                {
-                    array[i][j] = array[i + 1][j];
-                    array[i + 1][j] = 0;
-                }
-                if (array[i][j] == array[i + 1][j] && array[i][j] != 0 && arrayCheck[i] && arrayCheck[i + 1])
-                {
-                    array[i][j] *= 2;
-                    array[i + 1][j] = 0;
-                    arrayCheck[i] = false;
-                }
-            }
-            check = true;
-            for (int i = 0; i <= gameSize - 2; i++)
-            {
-                if ((array[i][j] == 0 && array[i + 1][j] != 0) || (array[i][j] == array[i + 1][j] && array[i][j] != 0 && arrayCheck[i] && arrayCheck[i + 1]))
-                {
-                    check = false;
-                    break;
-                }
-            }
-        }
-    }
-}
-void updateArrayDown()
-{
-    // cout << "down" << endl;
-    for (int j = 0; j < gameSize; j++)
-    {
-        vector<bool> arrayCheck(size_max, true);
-        bool check = false; // check cột hợp lệ chưa
-        while (!check)
-        {
-            for (int i = gameSize - 1; i > 0; i--)
-            {
-                if (array[i][j] == 0 && array[i - 1][j] != 0)
-                {
-                    array[i][j] = array[i - 1][j];
-                    array[i - 1][j] = 0;
-                }
-                if (array[i][j] == array[i - 1][j] && array[i][j] != 0 && arrayCheck[i] && arrayCheck[i - 1])
-                {
-                    array[i][j] *= 2;
-                    array[i - 1][j] = 0;
-                    arrayCheck[i] = false;
-                }
-            }
-            check = true;
-            for (int i = gameSize - 1; i > 0; i--)
-            {
-                if ((array[i][j] == 0 && array[i - 1][j] != 0) || (array[i][j] == array[i - 1][j] && array[i][j] != 0 && arrayCheck[i] && arrayCheck[i - 1]))
-                {
-                    check = false;
-                    break;
-                }
-            }
-        }
-    }
-}
-void updateArrayRight()
-{
-    // cout << "right" << endl;
-    for (int i = 0; i < gameSize; i++)
-    {
-        vector<bool> arrayCheck(size_max, true);
-        bool check = false; // check cột hợp lệ chưa
-        while (!check)
-        {
-            for (int j = gameSize - 1; j > 0; j--)
-            {
-                if (array[i][j] == 0 && array[i][j - 1] != 0)
-                {
-                    array[i][j] = array[i][j - 1];
-                    array[i][j - 1] = 0;
-                }
-                if (array[i][j] == array[i][j - 1] && array[i][j] != 0 && arrayCheck[j] && arrayCheck[j - 1])
-                {
-                    array[i][j] *= 2;
-                    array[i][j - 1] = 0;
-                    arrayCheck[j] = false;
-                }
-            }
-            check = true;
-            for (int j = gameSize - 1; j > 0; j--)
-            {
-                if ((array[i][j] == 0 && array[i][j - 1] != 0) || (array[i][j] == array[i][j - 1] && array[i][j] != 0 && arrayCheck[j] && arrayCheck[j - 1]))
-                {
-                    check = false;
-                    break;
-                }
-            }
-        }
-    }
-}
-void updateArrayLeft()
-{
-    // cout << "left" << endl;
-    for (int i = 0; i < gameSize; i++)
-    {
-        vector<bool> arrayCheck(size_max, true);
-        bool check = false; // check cột hợp lệ chưa
-        while (!check)
-        {
-            for (int j = 0; j <= gameSize - 2; j++)
-            {
-                if (array[i][j] == 0 && array[i][j + 1] != 0)
-                {
-                    array[i][j] = array[i][j + 1];
-                    array[i][j + 1] = 0;
-                }
-                if (array[i][j] == array[i][j + 1] && array[i][j] != 0 && arrayCheck[j] && arrayCheck[j + 1])
-                {
-                    array[i][j] *= 2;
-                    array[i][j + 1] = 0;
-                    arrayCheck[j] = false;
-                }
-            }
-            check = true;
-            for (int j = 0; j <= gameSize - 2; j++)
-            {
-                if ((array[i][j] == 0 && array[i][j + 1] != 0) || (array[i][j] == array[i][j + 1] && array[i][j] != 0 && arrayCheck[j] && arrayCheck[j + 1]))
-                {
-                    check = false;
-                    break;
-                }
-            }
-        }
-    }
-}
-bool compareArray(int (*arrayBefore)[size_max])
+void khoiTaoMang()
 {
     for (int i = 0; i < gameSize; i++)
     {
-        for (int j = 0; j < gameSize; j++)
-        {
-            if (arrayBefore[i][j] != array[i][j])
-                return false;
-        }
-    }
-    return true;
-}
-void updateArrayBefore(int (*arrayBerfore)[size_max])
-{
-    for (int i = 0; i < gameSize; i++)
-    {
-        for (int j = 0; j < gameSize; j++)
-        {
-            arrayBerfore[i][j] = array[i][j];
-        }
+        array[i]= new int [gameSize]();
+        arrayBefore[i]= new int [gameSize]();
     }
 }
