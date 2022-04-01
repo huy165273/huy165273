26ca03ae3222977723068fea0059967d20f609c3
huy165273 <21020765@vnu.edu.vn>
2022-04-02 01:00:54 +0700

    Bài Tập Lớn

--- Game2048/array_manipulation.h
+++ Game2048/array_manipulation.h
@@ -2,10 +2,12 @@
 #define ARRAY_MANIPULATION_H_INCLUDE
 
 
-void updateArrayUp(int **array, const int gameSize);
-void updateArrayDown(int **array, const int gameSize);
-void updateArrayRight(int **array, const int gameSize);
-void updateArrayLeft(int **array, const int gameSize); 
-void updateArrayBefore(int *(*arrayBerfore), int *(*array), const int gameSize);
+void updateArrayUp(int **array, const int gameSize, int &score);
+void updateArrayDown(int **array, const int gameSize, int &score);
+void updateArrayRight(int **array, const int gameSize, int &score);
+void updateArrayLeft(int **array, const int gameSize, int &score); 
+void updateArrayBefore(int *(*arrayBefore), int *(*array), const int gameSize);
 bool compareArray(int **arrayBefore, int **array, const int gameSize);
+void khoiTaoMang(int **array, int **arrayBefore, const int& gameSize);
+
 #endif
---
+++ Game2048/end_game.h
@@ -0,0 +1,4 @@
+
+bool gameOver(int **array, const int &gameSize);
+bool win(int **array, const int &gameSize);
+int hightScore(const int &score, int const& gameSize);
