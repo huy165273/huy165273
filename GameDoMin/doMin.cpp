e6b04f0ab41f48aa51b99fe43b347207b762c0d6
huy165273 <21020765@vnu.edu.vn>
2022-04-14 00:23:15 +0700

    Bài Tập lớn

--- GameDoMin/doMin.cpp
+++ GameDoMin/doMin.cpp
@@ -1,178 +1,241 @@
-#include<iostream>
-#include<string>
-#include<ctime>
-#include<cstdlib>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include "map.h"
 using namespace std;
-const int mMax=100, nMax=100;
-void enterArray(bool (*arr)[mMax], int (*map2)[nMax], int m, int n);
-void bombMap(bool (*arr)[mMax], int (*map)[mMax], int m, int n);
-bool checkBomb(int x, int y, bool (*arr)[mMax]);
-void editMap2( int x, int y, int (*map1)[mMax], int (*map2)[nMax], int m, int n);
-bool checkMap( int (*map1)[mMax], int (*map2)[nMax], int m, int n );
-void printMap(int (*map2)[mMax], int m, int n);
-void printBombMap(int (*map1)[mMax], int m, int n);
-void randomBombMap( bool (*arr)[mMax], int m, int n);
-void enterMap2(int (*map2)[mMax], int m, int n);
-void playBombDetection();
-int main(){
-    string str;
-    do{
-        playBombDetection();
-        cout<<"Play again (Yes/No): ";
-        cin>>str;
-    }while(str=="Yes");
+
+const int SIZE_WIDTH_MAX = 30;
+const int SIZE_HEIGHT_MAX = 15;
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 600;
+const int amount_image = 9;
+const int image_number_max = 7;
+
+bool init();
+bool loadMedia();
+void close();
+void showImage();
+
+SDL_Surface *loadImage(string path);
+SDL_Window *gWindow = NULL;
+SDL_Surface *gScreenSurface = NULL;           // bề mặt của cửa sổ
+SDL_Surface *(imageNumber[image_number_max]); // các ảnh số
+SDL_Surface *house = NULL;
+SDL_Surface *bom = NULL;
+SDL_Surface *co = NULL;
+
+int mapWidth = 10;
+int mapHeight = 10;
+int **map = new int *[SIZE_HEIGHT_MAX];
+int **showMap = new int *[SIZE_HEIGHT_MAX];
+void playGame();
+
+int main(int arc, char *argv[])
+{
+    playGame();
+    if (!init())
+    {
+        cout << "Failed to init." << endl;
+    }
+    else
+    {
+        if (!loadMedia())
+        {
+            cout << "Failed to load media." << endl;
+        }
+        else
+        {
+            bool quit = false;
+            bool check = true;
+            SDL_Event e;
+            while (!quit)
+            {
+                while (check)
+                {
+                    while (SDL_PollEvent(&e) != 0)
+                    {
+                        // User requests quit
+                        if (e.type == SDL_QUIT)
+                        {
+                            quit = true;
+                        }
+                        else
+                        {
+                            if (e.type == SDL_MOUSEBUTTONDOWN)
+                            {
+                                int x, y;
+                                SDL_GetMouseState(&x, &y);
+                                if (x >= 275 && x <= 725 && y >= 125 && y <= 575)
+                                {
+                                    x = (x - 275) / 45;
+                                    y = (y - 125) / 45;
+                                    if (checkBomb(y, x, map))
+                                    {
+                                        check = false;
+                                        cout << "You are dead! You lose!" << endl;
+                                    }
+                                    else
+                                    {
+                                        editShowMap(y, x, map, showMap, mapWidth, mapHeight);
+                                    }
+                                }
+                            }
+                        }
+                        showImage();
+                        SDL_UpdateWindowSurface(gWindow);
+                    }
+                    if(quit) break;
+                }
+            }
+        }
+    }
     return 0;
 }
-void playBombDetection(){
-    int m,n,x,y;
-    bool arr[mMax][nMax];
-    int map1[mMax][nMax],// bản đồ bom
-        map2[mMax][nMax];// bản đồ sau mỗi lần người chơi đoán 
-    cout<<"Enter map size: ";
-    cin>>m>>n;
-    randomBombMap(arr, m, n);
-    //enterArray(arr, map2, m, n);
-    bombMap(arr, map1, m, n);
-    bool check=true ; 
-    enterMap2(map2, m, n);
-    printMap(map2, m, n);
-    while(check){
-        cout<<"Enter the coordinates of your choice: ";
-        cin>>x>>y;
-        if(checkBomb(x, y, arr)==true){
-            check=false ;
-            cout<<"You are dead! You lose!"<<endl;
-            cout<<"Bomb map: "<<endl;
-            printBombMap(map1, m, n);
-        }
-        else{
-            editMap2(x, y, map1, map2, m, n);
-            printMap(map2, m, n);
-        }
-        if(checkMap(map1, map2, m, n)){
-            check=false;
-            cout<<"Congratulate! You win!"<<endl;
-            cout<<"Bomb map: "<<endl;
-            printBombMap(map1, m, n);
-        }
+bool init()
+{
+    bool success = true;
+    if (SDL_Init(SDL_INIT_VIDEO < 0))
+    {
+        success = false;
     }
-}
-void enterArray(bool (*arr)[mMax],int (*map2)[nMax],  int m, int n){
-    for(int i=1;i<=m;i++){
-        string str;
-        cin>>str;
-        for(int j=1;j<=n;j++) {
-            arr[i][j]=str[j-1];
-            map2[i][j]=-1;
+    else
+    {
+        gWindow = SDL_CreateWindow("Do Min", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            success = false;
         }
-    }
-    for(int i=0;i<=n;i++) map2[0][i]=i;
-    for(int i=1;i<=m;i++) map2[i][0]=i;
-}
-void bombMap(bool (*arr)[mMax], int (*map1)[mMax], int m, int n){
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
-           int count=0;
-            bool check=false;
-            for(int k=i-1;k<=i+1;k++){
-                for(int t=j-1;t<=j+1;t++){
-                    if(k==i && t==j){
-                        if(arr[k][t]==true) {
-                            count =-1;
-                            check=true;
-                            break;
-                        }
-                    } 
-                    else if(k>0 && k<=m && t>0 && t<=n){
-                        if(arr[k][t]==true) count++;
-                    }
-                }
-                if(check) break;
-            }
-            map1[i][j]=count;   
-        }
-    }
-    for(int i=0;i<=n;i++) map1[0][i]=i;
-    for(int i=1;i<=m;i++) map1[i][0]=i;
-}
-bool checkBomb(int x, int y, bool (*arr)[mMax]){
-    if(arr[x][y]==true) return true;
-    return false;
-}
-bool checkMap( int (*map1)[mMax], int (*map2)[nMax], int m, int n ){
-    for(int i=1;i<=m;i++)
-        for(int j=1;j<=n;j++)
-            if(map1[i][j]!=map2[i][j]) return false;
-    return true;
-}
-void editMap2( int x, int y, int (*map1)[mMax], int (*map2)[nMax], int m, int n){
-    if(map1[x][y]>0) map2[x][y]=map1[x][y];
-    else{
-        map2[x][y]=0;
-        for(int i=x-1;i<=x+1;i++)
-            for(int j=y-1;j<=y+1;j++){
-                if(i>0 && i<=m && j>0 && j<=n && (i!=x || j!=y)) 
-                    if(map2[i][j]==-1) editMap2(i, j, map1, map2, m, n);
-            }
-    }
-}
-void printMap(int (*map2)[mMax], int m, int n){
-    for(int i=0;i<=m;i++) {
-        for(int j=0;j<=n;j++){
-            if(i==0 && j==0) cout<<"    ";
-            else{
-                if(j==0) {
-                    if(map2[i][j]<10 && map2[i][j]>=0) cout<<" "<<map2[i][j]<<"  ";
-                    else cout<<map2[i][j]<<"  ";
-                }
-                else {
-                    if(map2[i][j]<10 && map2[i][j]>=0) cout<<" "<<map2[i][j]<<" ";
-                    else cout<<map2[i][j]<<" ";
-                }
+        else
+        {
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+            if (gScreenSurface == NULL)
+            {
+                success = false;
             }
         }
-        if(i==0) cout<<endl<<endl;
-        else cout<<endl;
     }
+    return success;
 }
-void printBombMap(int (*map1)[mMax], int m, int n){
-    for(int i=0;i<=m;i++) {
-        for(int j=0;j<=n;j++){
-            if(i==0 && j==0) cout<<"    ";
-            else{
-                if(j==0) {
-                    if(map1[i][j]<10 && map1[i][j]>=0) cout<<" "<<map1[i][j]<<"  ";
-                    else cout<<map1[i][j]<<"  ";
-                }
-                else {
-                    if(map1[i][j]!=-1) cout<<" "<<map1[i][j]<<" ";
-                    else cout<<" * ";
-                }
+void close()
+{
+    SDL_FreeSurface(house);
+    house = NULL;
+    SDL_FreeSurface(bom);
+    bom = NULL;
+    SDL_FreeSurface(co);
+    co = NULL;
+    for (int i = 0; i < image_number_max; i++)
+    {
+        SDL_FreeSurface(imageNumber[i]);
+        imageNumber[i] = NULL;
+    }
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    SDL_Quit();
+}
+bool loadMedia()
+{
+    bool success = true;
+    string path;
+    ifstream file;
+    file.open("file_image.txt");
+    if (!file)
+    {
+        cout << "Failed to open file." << endl;
+    }
+    else
+    {
+        file >> path;
+        house = loadImage(path);
+        file >> path;
+        bom = loadImage(path);
+        file >> path;
+        co = loadImage(path);
+        for (int i = 0; i < image_number_max; i++)
+        {
+            file >> path;
+            imageNumber[i] = loadImage(path);
+            if (imageNumber[i] == NULL)
+            {
+                success = false;
+                cout << "Failed to load image: " << path << endl;
             }
         }
-        if(i==0) cout<<endl<<endl;
-        else cout<<endl;
     }
+    return success;
 }
-void randomBombMap( bool (*arr)[mMax], int m, int n){
-    for(int i=1;i<=m;i++)
-        for(int j=1;j<=n;j++) arr[i][j]=false;
-    int count=1;
-    while(count<=m){
-        srand(time(nullptr));
-        int x=rand()%m + 1;
-        int y=rand()%n + 1;
-        if(arr[x][y]==false) {
-            arr[x][y]=true;
-            count++;
+SDL_Surface *loadImage(string path)
+{
+    SDL_Surface *optimizeSurface = NULL;
+    SDL_Surface *loadSurface = SDL_LoadBMP(path.c_str());
+    if (loadSurface != NULL)
+    {
+        optimizeSurface = SDL_ConvertSurface(loadSurface, gScreenSurface->format, 0);
+        SDL_FreeSurface(loadSurface);
+    }
+    else
+    {
+        cout << "Failed to load surface." << endl;
+    }
+    return optimizeSurface;
+}
+void playGame()
+{
+    int x, y;
+    mapInitialization(map, mapWidth, mapHeight);
+    showMapInitialization(showMap, mapWidth, mapHeight);
+    randomBombMap(map, mapWidth, mapHeight, 10);
+    numberInMap(map, mapWidth, mapHeight);
+    bool check = true;
+    /*while (check)
+    {
+        cout << "Enter the coordinates of your choice: ";
+        cin >> x >> y;
+        if (checkBomb(x, y, map))
+        {
+            check = false;
+            cout << "You are dead! You lose!" << endl;
+            cout << "Bomb map: " << endl;
+        }
+        else
+        {
+            editShowMap(x, y, map, showMap, mapWidth, mapHeight);
+        }
+        if (checkMap(map, showMap, mapWidth, mapHeight))
+        {
+            check = false;
+            cout << "Congratulate! You win!" << endl;
+            cout << "Bomb map: " << endl;
+        }
+    }*/
+}
+void showImage()
+{
+    SDL_Rect stretch;
+    stretch.x = 0;
+    stretch.y = 0;
+    stretch.h = SCREEN_HEIGHT;
+    stretch.w = SCREEN_WIDTH;
+    SDL_BlitScaled(house, NULL, gScreenSurface, &stretch);
+    for (int i = 0; i < mapHeight; i++)
+    {
+        for (int j = 0; j < mapWidth; j++)
+        {
+            stretch.x = 275 + 45 * j;
+            stretch.y = 125 + 45 * i;
+            stretch.w = 45;
+            stretch.h = 45;
+            if (map[i][j] >= 0 && map[i][j] <= 6)
+            {
+                SDL_BlitScaled(imageNumber[showMap[i][j]], NULL, gScreenSurface, &stretch);
+            }
+            else
+            {
+                SDL_BlitScaled(bom, NULL, gScreenSurface, &stretch);
+            }
         }
     }
-}
-void enterMap2(int (*map2)[mMax], int m, int n){
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++) map2[i][j]=-1;
-        map2[0][i]=i;
-        map2[i][0]=i;
-    }
-        
 }
